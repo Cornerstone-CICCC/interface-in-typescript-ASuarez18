bbb70ea4feabaf3ffefac739f0bff07936741189
Alejandro Suárez <alejandro.sg.1825@gmail.com>
2026-04-29 22:10:32 -0700

    ex01 finished

--- exercises/ex01.ts
+++ exercises/ex01.ts
@@ -1,1 +1,36 @@
 //Code here
+interface Triangle {
+  sideA: number;
+  sideB: number;
+  sideC: number;
+
+  calculatePerimeter(): number;
+  calculateArea(): number;
+  isTriangle(): boolean;
+}
+
+
+const myTriangle: Triangle = {
+  sideA: 3,
+  sideB: 4,
+  sideC: 5,
+
+  calculatePerimeter(): number {
+    return this.sideA + this.sideB + this.sideC;
+  },
+
+  calculateArea(): number {
+    const s = this.calculatePerimeter() / 2;
+    return Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
+  },
+
+  isTriangle(): boolean {
+    return (this.sideA + this.sideB > this.sideC &&
+      this.sideA + this.sideC > this.sideB &&
+      this.sideB + this.sideC > this.sideA);
+  }
+};
+
+console.log("Perimeter:", myTriangle.calculatePerimeter());
+console.log("Area:", myTriangle.calculateArea());
+console.log("Is it a triangle?", myTriangle.isTriangle());
